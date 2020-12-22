aa96bd97e257d4546992eae6140551b983dc1011
coufran <coufran@qq.com>
2020-12-22 15:52:44 +0800

    修改：account.list接口

--- src/main/java/cn/coufran/ve/api/AccountController.java
+++ src/main/java/cn/coufran/ve/api/AccountController.java
@@ -1,21 +1,30 @@
 package cn.coufran.ve.api;
 
+import cn.coufran.ve.api.vo.AccountVo;
 import cn.coufran.ve.model.Account;
 import cn.coufran.ve.service.AccountService;
+import cn.coufran.ve.util.AccountVoConvertor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/web/account")
 public class AccountController {
     @Resource
     private AccountService accountService;
+    @Resource
+    private AccountVoConvertor accountVoConvertor;
 
     @RequestMapping("/list")
-    public List<Account> list() {
-        return accountService.list();
+    public List<AccountVo> list() {
+        return accountService.list().stream()
+                .map(accountVoConvertor::from)
+                .collect(Collectors.toList());
     }
 }
