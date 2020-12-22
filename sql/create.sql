8e74b9f13a331c0cd99f06d905cb9405aa39084c
liuhuiming <liuhuiming@dotax.cn>
2020-12-22 22:03:58 +0800

    修改：建表SQL

--- sql/create.sql
+++ sql/create.sql
@@ -5,7 +5,7 @@ create table user(
   id int primary key auto_increment comment 'ID',
   username varchar(10) not null unique comment '用户名',
   password char(32) not null comment '密码'
-);
+) character set = utf8;
 
 /** Token 权限 */
 drop table if exists token;
@@ -14,7 +14,7 @@ create table token(
   token char(32) primary key comment 'Token值',
   user_id int not null comment '用户ID',
   expire long not null comment '过期时间'
-);
+) character set = utf8;
 
 /** 收支记录 */
 drop table if exists record;
@@ -27,7 +27,7 @@ create table record(
   group_id int not null comment '联合ID',
   time datetime not null comment '时间',
   remark varchar(100) comment '备注'
-);
+) character set = utf8;
 
 /** 账户 */
 drop table if exists account;
@@ -37,7 +37,7 @@ create table account(
   name varchar(20) not null comment '名称',
   amount int not null comment '余额',
   title_id int not null comment '所属科目'
-);
+) character set = utf8;
 
 /** 科目 */
 drop table if exists title;
@@ -46,4 +46,4 @@ create table title(
   id int primary key auto_increment comment 'ID',
   name varchar(20) not null comment '名称',
   kind enum('ASSETS', 'LIABILITY', 'PROFIT', 'LOSS') not null comment '分类'
-);
+) character set = utf8;
---
+++ sql/init_test.sql
@@ -0,0 +1,16 @@
+insert into user values(1, 'coufran', '6eed84a9f80ad64ec927ec0012e2602a');
+
+insert into title values(1, '现金', 'ASSETS');
+insert into title values(2, '信用卡', 'LIABILITY');
+insert into title values(3, '收入', 'PROFIT');
+insert into title values(4, '支出', 'LOSS');
+
+insert into account values(1, '钱包', 10000, 1);
+insert into account values(2, '招商银行信用卡', 50000, 2);
+insert into account values(3, '晋商银行信用卡', 30000, 2);
+insert into account values(4, '工资', 0, 3);
+insert into account values(5, '餐饮', 15000, 4);
+
+insert into record values(1, 1000000, 1, 4, 1, now(), '1月工资');
+insert into record values(1, 1100, 5, 1, 2, now(), '午饭');
+insert into record values(1, 10000, 1, 2, 3, now(), '还款');
