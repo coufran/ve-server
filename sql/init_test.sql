599c2af361553fcb1bdc8f0df6980f7fe798c573
coufran <coufran@qq.com>
2021-01-13 18:05:20 +0800

    增加：测试数据

--- sql/init_test.sql
+++ sql/init_test.sql
@@ -10,6 +10,9 @@ insert into account values(2, '招商银行信用卡', 50000, 2);
 insert into account values(3, '晋商银行信用卡', 30000, 2);
 insert into account values(4, '工资', 0, 3);
 insert into account values(5, '餐饮', 15000, 4);
+insert into account values(6, '交通', 0, 4);
+insert into account values(7, '购物', 0, 4);
+insert into account values(8, '娱乐', 0, 4);
 
 insert into record values(1, 1000000, 1, 4, 1, now(), '1月工资');
 insert into record values(1, 1100, 5, 1, 2, now(), '午饭');
--- src/main/java/cn/coufran/ve/api/TitleController.java
+++ src/main/java/cn/coufran/ve/api/TitleController.java
@@ -2,6 +2,7 @@ package cn.coufran.ve.api;
 
 import cn.coufran.ve.model.Title;
 import cn.coufran.ve.service.TitleService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/web/title")
+@CrossOrigin
 public class TitleController {
 
     @Resource
